aa27250e96fc49c19843bd560aee890662aed25d
asforest <asforest@qq.com>
2022-03-18 20:52:41 +0800

    optimize batch script locating policy

--- contents/start.bat
+++ contents/start.bat
@@ -2,11 +2,23 @@
 cls
 cd %_lw_exedir%
 
-SET file_name="updater-script.bat"
+SET file_name=updater-script.bat
+SET file_path=%file_name%
 
-if exist %file_name% (
-    call %file_name%
+if not exist %file_path% (
+    SET file_path=".minecraft/updater-script.bat"
+)
+
+if not exist %file_path% (
+    SET file_path=".minecraft/updater/updater-script.bat"
+)
+
+if exist %file_path% (
+    call %file_path%
 ) else (
-    echo %file_name% was found in directory %cd%
-    pause
+    echo %file_name% was found in one of the paths below:
+    echo   - %cd%/%file_name%
+    echo   - %cd%.minecraft/%file_name%
+    echo   - %cd%.minecraft/updater/%file_name%
+    ping 127.0.0.1 -n 5 > nul
 )
--- pack.bat
+++ pack.bat
@@ -1,2 +1,2 @@
 @echo off
-lw-v2.0.0-rc1-Release-x64.exe -p contents --exec="$_lw_tempdir\start.bat" --output=output.exe
+lw-v2.0.0-rc1-Release-x64.exe -p contents --exec="$_lw_tempdir\start.bat" --output=batch-runner.exe
